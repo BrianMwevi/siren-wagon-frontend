44ea12ed08cc80c4f0dd97525023983eff08f135
lcmongwe <lcmongwe@gmail.com>
2022-07-04 14:56:50 +0300

    create components ambulances and payments

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { PackagesComponent } from './packages/packages.component';
+import { HomeComponent } from './components/home/home.component';
+import { PackagesComponent } from './components/packages/packages.component';
+import { AmbulancesComponent } from './components/ambulances/ambulances.component';
+import { PaymentsComponent } from './components/payments/payments.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  {path: 'packages', component: PackagesComponent}
+  { path: 'packages', component: PackagesComponent },
+  { path: 'ambulances', component: AmbulancesComponent },
+  { path:'payments', component: PaymentsComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { PackagesComponent } from './packages/packages.component';
+import { HomeComponent } from './components/home/home.component';
+import { PackagesComponent } from './components/packages/packages.component';
+import { AmbulancesComponent } from './components/ambulances/ambulances.component';
+import { PaymentsComponent } from './components/payments/payments.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    PackagesComponent
+    PackagesComponent,
+    AmbulancesComponent,
+    PaymentsComponent
   ],
   imports: [
     BrowserModule,
