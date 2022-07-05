90eef65f7070cbb36a8eb4f3a87e88ee628ad012
juliamwangi123 <julia.mwangi@student.moringaschool.com>
2022-07-05 11:13:25 +0300

    change home page path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,21 +5,18 @@ import { HomeComponent } from './components/home/home.component';
 import { PackagesComponent } from './components/packages/packages.component';
 import { AmbulancesComponent } from './components/ambulances/ambulances.component';
 import { PaymentsComponent } from './components/payments/payments.component';
-import{LandingComponent} from './landing/landing.component'
+import { LandingComponent } from './components/landing/landing.component';
 
-
-
-  const routes: Routes = [
+const routes: Routes = [
   { path: '', component: LandingComponent },
   { path: '', component: HomeComponent },
   { path: 'packages', component: PackagesComponent },
   { path: 'ambulances', component: AmbulancesComponent },
-  { path:'payments', component: PaymentsComponent },
-  ]
-
+  { path: 'payments', component: PaymentsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ProfileComponent } from './profile/profile.component';
-import { NavComponent } from './nav/nav.component';
-import { LandingComponent } from './landing/landing.component';
+import { NavComponent } from './components/nav/nav.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { LandingComponent } from './components/landing/landing.component';
 import { HomeComponent } from './components/home/home.component';
 import { PackagesComponent } from './components/packages/packages.component';
 import { AmbulancesComponent } from './components/ambulances/ambulances.component';
@@ -16,19 +16,15 @@ import { AuthComponent } from './components/auth/auth.component';
   declarations: [
     AppComponent,
     NavComponent,
-    LandingComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
+    LandingComponent,
     ProfileComponent,
-    NavComponent,
     HomeComponent,
     PackagesComponent,
     AmbulancesComponent,
     PaymentsComponent,
     AuthComponent,
   ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
