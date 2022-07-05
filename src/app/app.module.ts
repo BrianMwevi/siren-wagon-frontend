eb64d1888ac8a8dcc3c9fd608ffb486a673f2c03
lcmongwe <lcmongwe@gmail.com>
2022-07-05 12:27:32 +0300

    register nav and landing component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
-import { ProfileComponent } from './components/profile/profile.component';
-import { NavComponent } from './components/nav/nav.component';
-=======
 import { NavComponent } from './components/nav/nav.component';
 import { ProfileComponent } from './components/profile/profile.component';
 import { LandingComponent } from './components/landing/landing.component';
->>>>>>> dda68e9c92e8e5efc1cc22e43b0be96ee5d66b77
 import { HomeComponent } from './components/home/home.component';
 import { PackagesComponent } from './components/packages/packages.component';
 import { AmbulancesComponent } from './components/ambulances/ambulances.component';
