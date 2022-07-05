5f12877ba49dfb56c48f5c5b70cabb54bbefc1bb
lcmongwe <lcmongwe@gmail.com>
2022-07-05 10:38:55 +0300

    move nav and profile into components folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ProfileComponent } from './profile/profile.component';
-import { NavComponent } from './nav/nav.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { NavComponent } from './components/nav/nav.component';
 import { HomeComponent } from './components/home/home.component';
 import { PackagesComponent } from './components/packages/packages.component';
 import { AmbulancesComponent } from './components/ambulances/ambulances.component';
