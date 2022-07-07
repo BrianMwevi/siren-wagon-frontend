956705b6fe2eb17379272bb84e02c31c55872bfa
juliamwangi123 <julia.mwangi@student.moringaschool.com>
2022-07-07 10:22:27 +0300

    fetch trips from trip endpoint

--- src/app/components/payments/payments.component.ts
+++ src/app/components/payments/payments.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { TripService } from '../../services/trip.service';
+
 
 @Component({
   selector: 'app-payments',
   templateUrl: './payments.component.html',
-  styleUrls: ['./payments.component.css']
+  styleUrls: ['./payments.component.css'],
 })
 export class PaymentsComponent implements OnInit {
+  trip: any = [];
 
-  constructor() { }
+  constructor(private tripservice: TripService) {}
 
   ngOnInit(): void {
+    this.trips();
+
   }
 
+  trips(): void {
+    this.tripservice.getTrip().subscribe((data) => {
+      this.trip=data
+      console.log(this.trip)
+
+    });
+  }
 }
