75ba1d926ce49bf758fbb28877c0ee89821dea97
brianmwevi <mwevibrian@gmail.com>
2022-07-08 11:11:34 +0300

    ft:dependacies -- add ng-loader and flash messages configs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { HttpClientModule } from '@angular/common/http';
-
+import { FlashMessagesModule } from 'flash-messages-angular';
 import { AppRoutingModule } from './app-routing.module';
+import {
+  NgxUiLoaderModule,
+  NgxUiLoaderConfig,
+  NgxUiLoaderRouterModule,
+  SPINNER,
+  POSITION,
+  PB_DIRECTION,
+} from 'ngx-ui-loader';
+
 import { AppComponent } from './app.component';
 import { NavComponent } from './components/nav/nav.component';
 import { ProfileComponent } from './components/profile/profile.component';
@@ -17,6 +26,16 @@ import { LoginComponent } from './components/login/login.component';
 import { AuthService } from './services/auth.service';
 import { RequestsInterceptor } from './interceptors/requests.interceptor';
 import { PaymentsFormComponent } from './components/payments-form/payments-form.component';
+
+const ngxUiLoaderConfig: NgxUiLoaderConfig = {
+  bgsColor: 'green',
+  bgsPosition: POSITION.bottomCenter,
+  bgsSize: 40,
+  bgsType: SPINNER.rectangleBounce, // background spinner type
+  fgsType: SPINNER.threeStrings, // foreground spinner type
+  pbDirection: PB_DIRECTION.leftToRight, // progress bar direction
+  pbThickness: 2, // progress bar thickness
+};
 
 @NgModule({
   declarations: [
@@ -32,7 +51,20 @@ import { PaymentsFormComponent } from './components/payments-form/payments-form.
     LoginComponent,
     PaymentsFormComponent,
   ],
-  imports: [BrowserModule, HttpClientModule, AppRoutingModule],
+
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule,
+    FlashMessagesModule.forRoot(),
+    // Import NgxUiLoaderModule with custom configuration globally
+    NgxUiLoaderModule.forRoot(ngxUiLoaderConfig),
+
+    // Import NgxUiLoaderModule
+    // NgxUiLoaderModule,
+    NgxUiLoaderRouterModule,
+    // NgxUiLoaderHttpModule,
+  ],
   providers: [
     AuthService,
     {
