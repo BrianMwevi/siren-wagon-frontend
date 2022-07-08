4404fb255c3779844958e0b2252a3ea842803b10
juliamwangi123 <julia.mwangi@student.moringaschool.com>
2022-07-08 08:07:42 +0300

    consuming trip endpoint

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { PackagesComponent } from './components/packages/packages.component';
 import { AmbulancesComponent } from './components/ambulances/ambulances.component';
 import { PaymentsComponent } from './components/payments/payments.component';
 import { LandingComponent } from './components/landing/landing.component';
+import  {ProfileComponent} from './components/profile/profile.component'
 
 const routes: Routes = [
   { path: '', component: LandingComponent },
@@ -13,6 +14,7 @@ const routes: Routes = [
   { path: 'packages', component: PackagesComponent },
   { path: 'ambulances', component: AmbulancesComponent },
   { path: 'payments', component: PaymentsComponent },
+  { path: 'profile', component: ProfileComponent },
 ];
 
 @NgModule({
