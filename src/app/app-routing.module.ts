eb4e5e7cdb67e35b5fa840e1e7648967cb51f0f0
brianmwevi <mwevibrian@gmail.com>
2022-07-10 00:55:50 +0300

    patch:guards -- add auth and not logged in guards

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,16 +9,38 @@ import { LandingComponent } from './components/landing/landing.component';
 import { ProfileComponent } from './components/profile/profile.component';
 import { SignupComponent } from './components/signup/signup.component';
 import { LoginComponent } from './components/login/login.component';
+import { AuthGuard } from './guards/auth.guard';
+import { NotLoggedInGuard } from './guards/not-logged-in.guard';
 
 const routes: Routes = [
   { path: '', component: LandingComponent },
-  { path: '', component: HomeComponent },
-  { path: 'signup', component: SignupComponent },
-  { path: 'login', component: LoginComponent },
-  { path: 'packages', component: PackagesComponent },
+  { path: 'home', component: HomeComponent },
+  {
+    path: 'signup',
+    component: SignupComponent,
+    canActivate: [NotLoggedInGuard],
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+    canActivate: [NotLoggedInGuard],
+  },
+  {
+    path: 'packages',
+    component: PackagesComponent,
+    canActivate: [AuthGuard],
+  },
   { path: 'ambulances', component: AmbulancesComponent },
-  { path: 'payments', component: PaymentsComponent },
-  { path: 'profile', component: ProfileComponent },
+  {
+    path: 'payments',
+    component: PaymentsComponent,
+    canActivate: [AuthGuard],
+  },
+  {
+    path: 'profile',
+    component: ProfileComponent,
+    canActivate: [AuthGuard],
+  },
 ];
 
 @NgModule({
