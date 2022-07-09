2bb186d468d590a610284faf59f3ed570389fd4d
brianmwevi <mwevibrian@gmail.com>
2022-07-09 19:28:19 +0300

    ch:dependancies -- add ng loader

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,9 @@ import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { HttpClientModule } from '@angular/common/http';
 import { FlashMessagesModule } from 'flash-messages-angular';
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+
 import {
   NgxUiLoaderModule,
   NgxUiLoaderConfig,
@@ -13,6 +16,7 @@ import {
   PB_DIRECTION,
 } from 'ngx-ui-loader';
 
+import { NgxIntlTelInputModule } from 'ngx-intl-tel-input';
 import { AppComponent } from './app.component';
 import { NavComponent } from './components/nav/nav.component';
 import { ProfileComponent } from './components/profile/profile.component';
@@ -26,6 +30,9 @@ import { LoginComponent } from './components/login/login.component';
 import { AuthService } from './services/auth.service';
 import { RequestsInterceptor } from './interceptors/requests.interceptor';
 import { PaymentsFormComponent } from './components/payments-form/payments-form.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 const ngxUiLoaderConfig: NgxUiLoaderConfig = {
   bgsColor: 'green',
@@ -55,7 +62,10 @@ const ngxUiLoaderConfig: NgxUiLoaderConfig = {
   imports: [
     BrowserModule,
     HttpClientModule,
-    AppRoutingModule,
+    NgxIntlTelInputModule,
+    ReactiveFormsModule,
+    FormsModule,
+    BrowserAnimationsModule,
     FlashMessagesModule.forRoot(),
     // Import NgxUiLoaderModule with custom configuration globally
     NgxUiLoaderModule.forRoot(ngxUiLoaderConfig),
@@ -64,6 +74,9 @@ const ngxUiLoaderConfig: NgxUiLoaderConfig = {
     // NgxUiLoaderModule,
     NgxUiLoaderRouterModule,
     // NgxUiLoaderHttpModule,
+    AppRoutingModule,
+    MatFormFieldModule,
+    MatInputModule,
   ],
   providers: [
     AuthService,
