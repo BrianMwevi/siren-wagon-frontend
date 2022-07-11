1359b75f6f204edc102ecfe1bc2f8554419738cf
Aliyo254 <alinurali254@gmail.com>
2022-07-11 11:29:12 +0300

    1st

--- src/app/components/packages/packages.component.ts
+++ src/app/components/packages/packages.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
-
+import { PackageService } from 'src/app/services/package.service';
 @Component({
   selector: 'app-packages',
   templateUrl: './packages.component.html',
   styleUrls: ['./packages.component.css']
 })
 export class PackagesComponent implements OnInit {
+  package:any =[]
 
-  constructor() { }
+  constructor(private package__service : PackageService) { }
 
-  ngOnInit(): void {
+ payment(): void{
+   this.package__service.getPackages().subscribe((data)=>{
+     this.package = data
+
+     console.log(this.package)
+    })
   }
-
-}
+ 
+   ngOnInit(): void {
+     this.payment() 
+   }
+ 
+ }
---
+++ src/app/services/package.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+
+
+
+
+
+
+
+
+
+@Injectable({
+    providedIn: 'root'
+  })
+  export class PackageService {
+  
+    url = environment.DEV_URL
+   
+    constructor(private http : HttpClient) { }
+  
+    getPackages(): Observable <any>{
+      return this.http.get(`${this.url}/packages`)
+    }
+  
+  
+    
+  }
